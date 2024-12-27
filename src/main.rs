4328b7075ab9b3cb422bf034dfe6395cba5af8c1
Simon Cornelius <simonjoecornelius@gmail.com>
2024-12-27 15:20:35 +0100

    feat: artificially errored arrays
    also fix the parity bit calc

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,24 @@ fn calculate_parity_bit(msg: [u8; 16]) -> bool {
     for i in msg {
         sum += i;
     }
-    sum % 2 == 0
+    sum % 2 == 1
 }
 
 fn main() {
     let even_message: [u8; 16] = [0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0];
     let odd_message: [u8; 16] = [0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0];
+
+    let errored_even_message = {
+        let mut msg = even_message;
+        msg[10] ^= 1;
+        msg
+    };
+
+    let errored_odd_message = {
+        let mut msg = odd_message;
+        msg[10] ^= 1;
+        msg
+    };
 
     println!(
         "parity for even message: {}",
@@ -17,5 +29,13 @@ fn main() {
     println!(
         "parity for odd message: {}",
         calculate_parity_bit(odd_message),
+    );
+    println!(
+        "parity for errored even message: {}",
+        calculate_parity_bit(errored_even_message),
+    );
+    println!(
+        "parity for errored odd message: {}",
+        calculate_parity_bit(errored_odd_message),
     );
 }
